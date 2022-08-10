08c4393cb3d1ed6946aa24f47c4ee9159a5be8d7
squaresmile <squaresmile@protonmail.com>
2022-08-10 09:47:49 +0700

    Added google play version parser

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,6 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import json5 from "json5";
 
-export interface Env {
-    // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-    // MY_KV_NAMESPACE: KVNamespace;
-    //
-    // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-    // MY_DURABLE_OBJECT: DurableObjectNamespace;
-    //
-    // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-    // MY_BUCKET: R2Bucket;
-}
+export interface Env {}
 
 export default {
     async fetch(
@@ -25,6 +8,53 @@ export default {
         env: Env,
         ctx: ExecutionContext
     ): Promise<Response> {
-        return new Response("Hello World!");
+        const packageId = new URL(request.url).searchParams.get("id");
+        if (packageId === null) {
+            return new Response(
+                "id parameter required. For example: ?id=com.google.android.play.games",
+                { status: 400 }
+            );
+        }
+
+        const gplayResponse = await fetch(
+            `https://play.google.com/store/apps/details?id=${packageId}`
+        );
+        if (gplayResponse.status !== 200) {
+            return new Response(
+                `Google Play page returns status code ${gplayResponse.status} ${gplayResponse.statusText}`,
+                { status: 500 }
+            );
+        }
+
+        const siteText = await gplayResponse.text();
+        const matches = siteText.matchAll(
+            /<script nonce=\"\S+\">AF_initDataCallback\((.*?)\);/g
+        );
+        if (matches !== null) {
+            for (const match of matches) {
+                const data = json5.parse(match[1]);
+                try {
+                    const play_store_version = data["data"][1];
+                    if (/^\d+\.\d+\.\d+$/.test(play_store_version)) {
+                        return new Response(play_store_version, {
+                            status: 200,
+                        });
+                    }
+                } catch {}
+
+                try {
+                    const play_store_version = data["data"][1][2][140][0][0][0];
+                    if (/^\d+\.\d+\.\d+$/.test(play_store_version)) {
+                        return new Response(play_store_version, {
+                            status: 200,
+                        });
+                    }
+                } catch {}
+            }
+        }
+
+        return new Response(`Can't parse version from Google Play page`, {
+            status: 500,
+        });
     },
 };
