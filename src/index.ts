d68595a6b7d9aaf190b2066bb01a6dfef3e415b2
squaresmile <squaresmile@protonmail.com>
2022-08-10 10:05:34 +0700

    Removed version regex requirement since other apps can have different version schema

--- src/index.ts
+++ src/index.ts
@@ -33,21 +33,10 @@ export default {
         for (const match of matches) {
             const data = json5.parse(match[1]);
             try {
-                const play_store_version = data["data"][1];
-                if (/^\d+\.\d+\.\d+$/.test(play_store_version)) {
-                    return new Response(play_store_version, {
-                        status: 200,
-                    });
-                }
-            } catch {}
-
-            try {
                 const play_store_version = data["data"][1][2][140][0][0][0];
-                if (/^\d+\.\d+\.\d+$/.test(play_store_version)) {
-                    return new Response(play_store_version, {
-                        status: 200,
-                    });
-                }
+                return new Response(play_store_version, {
+                    status: 200,
+                });
             } catch {}
         }
 
